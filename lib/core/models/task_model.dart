1a45b993ac4ea15cef06bb4fe81e57ca30c01efa
Md Ikhtiar Alam <mdikhtiaralam@gmail.com>
2025-12-12 21:17:12 +0600

    refactor: add copyWith to TaskModel

--- lib/core/models/task_model.dart
+++ lib/core/models/task_model.dart
@@ -36,4 +36,56 @@ class TaskModel extends HiveObject {
     this.parentTaskId,
     this.status = "pending",
   });
+
+  TaskModel copyWith({
+    String? title,
+    String? dueDate,
+    String? id,
+    String? dueTime,
+    double? latitude,
+    double? longitude,
+    String? parentTaskId,
+    String? status,
+  }) {
+    return TaskModel(
+      title: title ?? this.title,
+      dueDate: dueDate ?? this.dueDate,
+      id: id ?? this.id,
+      dueTime: dueTime ?? this.dueTime,
+      latitude: latitude ?? this.latitude,
+      longitude: longitude ?? this.longitude,
+      parentTaskId: parentTaskId ?? this.parentTaskId,
+      status: status ?? this.status,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'dueDate': dueDate,
+      'id': id,
+      'dueTime': dueTime,
+      'latitude': latitude,
+      'longitude': longitude,
+      'parentTaskId': parentTaskId,
+      'status': status,
+    };
+  }
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      title: map['title'] ?? '',
+      dueDate: map['dueDate'],
+      id: map['id'] ?? '',
+      dueTime: map['dueTime'],
+      latitude: map['latitude'] != null
+          ? (map['latitude'] as num).toDouble()
+          : null,
+      longitude: map['longitude'] != null
+          ? (map['longitude'] as num).toDouble()
+          : null,
+      parentTaskId: map['parentTaskId'],
+      status: map['status'] ?? 'pending',
+    );
+  }
 }
