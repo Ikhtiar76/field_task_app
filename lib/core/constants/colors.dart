7b3976c08352a8f7a3d27f72b3056102e95484d6
Md Ikhtiar Alam <mdikhtiaralam@gmail.com>
2025-12-12 22:03:54 +0600

    refactor: introduce color constants and update UI to use them

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color green = Colors.green;
+const Color blue = Colors.blue;
+const Color grey = Color.fromARGB(255, 94, 93, 93);
+const Color orange = Colors.orange;
+const Color red = Colors.red;
